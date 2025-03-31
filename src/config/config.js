907ae52f8dca26a9e4a877b6f6b41b44050ca0b9
multi-pass-ci <multi.pass.ci@gmail.com>
2025-03-30 19:59:10 -0600

    segunda correcion dependencia api iot

--- src/config/config.js
+++ src/config/config.js
@@ -1,19 +1,17 @@
 import { config } from 'dotenv';
-
 config();
 
-export const PORT = process.env.PORT || 4000
+export const PORT = process.env.PORT || 4000;
 
-export const DB_USER = process.env.DB_USER || "jonathan"
-export const DB_PASSWORD = process.env.DB_PASSWORD || "admin"
-export const DB_HOST = process.env.DB_HOST || "localhost"
-export const DB_DATABASE = process.env.DB_DATABASE || "sp_data"
-export const DB_PORT = process.env.DB_PORT || 3307
-export const TOKEN_SECRET = process.env.TOKEN_SECRET || "tokenrandom"; // Asegúrate de tener un valor en .env
-
-dotenv.config();
+export const DB_USER = process.env.DB_USER || "jonathan";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "admin";
+export const DB_HOST = process.env.DB_HOST || "localhost";
+export const DB_DATABASE = process.env.DB_DATABASE || "sp_data";
+export const DB_PORT = process.env.DB_PORT || 3307;
+export const TOKEN_SECRET = process.env.TOKEN_SECRET || "tokenrandom";
 
 export const MONGODB_URI = process.env.MONGODB_URI;
+
 
 // export const PORT = process.env.PORT || 4000;
 // export const DB_USER = process.env.DB_USER || "root";
