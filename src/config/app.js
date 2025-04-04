ccce9a6b35f37b7a617caec81ff4b03c8c4da835
multi-pass-ci <multi.pass.ci@gmail.com>
2025-04-04 00:17:21 -0600

    agregar ruta al cors 2

--- src/config/app.js
+++ src/config/app.js
@@ -17,7 +17,7 @@ app.use(
         origin: [
             'http://localhost:5173', 
             'http://localhost:8081',
-            'https://smartparking-eight.vercel.app/'
+            'https://smartparking-eight.vercel.app'
             ],
         credentials: true,
     })
