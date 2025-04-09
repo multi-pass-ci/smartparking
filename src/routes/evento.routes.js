bc60c6ad2712e7c2e14bb82eefd226bebf94475f
multi-pass-ci <multi.pass.ci@gmail.com>
2025-04-08 23:38:16 -0600

    ultimo deploy final

--- src/routes/evento.routes.js
+++ src/routes/evento.routes.js
@@ -1,17 +1,18 @@
+import { Router } from 'express';
 import {
-    registrarEvento,
-    listarEventos,
-    buscarEventoPorId,
-    editarEvento,
-    borrarEvento
-  } from '../controllers/evento.controller.js';
-  
+  registrarEvento,
+  listarEventos,
+  buscarEventoPorId,
+  editarEvento,
+  borrarEvento
+} from '../controllers/evento.controller.js';
+
 const router = Router();
 
-router.get('/eventos/:id', buscarEventoPorId); // Buscar evento por ID
-router.get('/eventos', listarEventos);      // Ver todos
-router.post('/eventos', registrarEvento);   // Crear evento
-router.put('/eventos/:id', editarEvento);      // Editar
-router.delete('/eventos/:id', borrarEvento);   // Eliminar
+router.post('/eventos', registrarEvento);
+router.get('/eventos', listarEventos);
+router.get('/eventos/:id', buscarEventoPorId);
+router.put('/eventos/:id', editarEvento);
+router.delete('/eventos/:id', borrarEvento);
 
 export default router;
